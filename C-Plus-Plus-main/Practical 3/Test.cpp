8cf04b7780fe25053e93eef0c025f94330c25865
dheeraj-07 <86804403+dheeraj-07@users.noreply.github.com>
2021-10-05 22:55:54 +0530

    Add files via upload

---
+++ C-Plus-Plus-main/Practical 3/Test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char C;
+	cout << "Enter the character: ";
+	cin >> C;
+	cout << C << endl;
+	return 0;
+}
---
+++ C-Plus-Plus-main/Practical 4/Test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	int a;
+	float b;
+	std::cin >> a >> b;
+	std::cout <<  a + (float)b << std::endl;
+	return 0;
+}
