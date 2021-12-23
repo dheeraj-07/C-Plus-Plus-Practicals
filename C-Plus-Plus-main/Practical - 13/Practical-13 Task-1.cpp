205a61c706b3c3a1d3d3c815066f1815c0b0c5c2
dheeraj-07 <86804403+dheeraj-07@users.noreply.github.com>
2021-12-23 09:56:19 +0530

    Create Practical-13 Task-1.cpp

---
+++ C-Plus-Plus-main/Practical - 13/Practical-13 Task-1.cpp
@@ -0,0 +1,19 @@
+#include"iostream"
+using namespace std;
+
+class base {
+    public:
+    int add(int a,int b) { return (a + b); }
+    float add(float a,float b,float c) { return (a + b + c); }
+};
+
+class child:public base {
+    public:
+    int add(int a,int b) { return (a + b + 1); }
+};
+
+int main() {
+child c;
+cout<<c.add(1.1,1.2)<<endl;
+cout<<c.add(1,2)<<endl;
+}
