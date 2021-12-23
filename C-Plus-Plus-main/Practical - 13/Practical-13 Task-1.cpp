43ead9ba69509b9d6de1f2765ae33e1d421fbc7d
dheeraj-07 <86804403+dheeraj-07@users.noreply.github.com>
2021-12-23 09:59:07 +0530

    Update Practical-13 Task-1.cpp

--- C-Plus-Plus-main/Practical - 13/Practical-13 Task-1.cpp
+++ C-Plus-Plus-main/Practical - 13/Practical-13 Task-1.cpp
@@ -5,6 +5,7 @@ class base {
     public:
     int add(int a,int b) { return (a + b); }
     float add(float a,float b,float c) { return (a + b + c); }
+    int add(int a,float b) { return (a + b); }
 };
 
 class child:public base {
@@ -16,4 +17,5 @@ int main() {
 child c;
 cout<<c.add(1.1,1.2)<<endl;
 cout<<c.add(1,2)<<endl;
+cout<<c.add(1,1.6);
 }
